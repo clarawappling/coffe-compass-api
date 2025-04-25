8e4867b18fad0e2f0c2af3d3206b7669427045e6
CLARA WÄPPLING <clara.wappling@gmail.com>
2025-04-25 22:14:32 +0200

    all endpoints for coffees added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import express from "express";
+import cors from "cors";
 
 let url = "mongodb://localhost:27017"
 
@@ -12,6 +13,7 @@ let connect = async () => {
     
     // Middleware
     server.use(express.json());
+    server.use(cors());
     
     // Collections
     let coffeesCollection = database.collection("coffees");
@@ -24,6 +26,48 @@ let connect = async () => {
     server.get("/coffees", async (req, res) => {
         let result = await coffeesCollection.find().toArray();
         res.send(result);
+    })
+
+    server.get("/coffees/:id", async (req, res) => {
+        let { id } = req.params;
+        let query = {_id: new ObjectId(id)};
+        let result = await coffeesCollection.findOne(query);
+        res.send(result);
+    })
+
+
+    server.post("/coffees", async (req, res) => {
+        let {name, description, origin, roast, arabica, roastery_id, img_src} = req.body;
+        let query = {name, description, origin, roast, arabica, roastery_id, img_src};
+        let result = await coffeesCollection.insertOne(query);
+
+        res.status(201).json({
+            message: "Coffee added",
+            coffeId: result.insertedId
+        })
+    })
+
+
+
+    server.patch("/coffees/:id", async (req, res) => {
+        let { id } = req.params;
+        let {name, description, origin, roast, arabica, roastery_id, img_src} = req.body;
+        let query = { _id: new ObjectId(id)}
+        let result = await coffeesCollection.updateOne(query, {$set: {name, description, origin, roast, arabica, roastery_id, img_src}})
+        res.status(201).json({
+            message: "Coffee updated",
+            updatedCount: result.modifiedCount
+        })
+    })
+
+    server.delete("/coffees/:id", async (req, res) => {
+        let { id } = req.params;
+        let query = {_id: new ObjectId(id)}
+        let result = await coffeesCollection.deleteOne(query);
+        res.status(200).json({
+            message: "Coffee removed successfully",
+            deletedCount: result.deletedCount
+        })
     })
     
     server.listen(4000, () => {
